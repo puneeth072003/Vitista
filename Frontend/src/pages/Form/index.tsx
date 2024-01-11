f1c04665641b63abefbb761ead3e52cbf5a261a7
Soorya U <sooryau7@gmail.com>
2024-01-11 23:46:25 +0530

    fix #3: Modified Form Component with Styles
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- Frontend/src/pages/Form/index.tsx
+++ Frontend/src/pages/Form/index.tsx
@@ -1,21 +1,15 @@
-import { FormEvent, useState } from "react";
+import { useEffect, useState } from "react";
 
-import axios from "axios";
-
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 import { addDays } from "date-fns";
 import { DateRange } from "react-day-picker";
 
-import { toast } from "sonner";
+import TabletForm from "@/components/custom/TabletForm";
+import axios from "axios";
 
-import FormDateRange from "@/components/custom/FormDateRange";
-import FormSelect from "@/components/custom/FormSelect";
-import FormTimePicker from "@/components/custom/FormTimePicker";
-
-import logo from "@/assets/icon.png";
-import { Button } from "@/components/ui/button";
+import "./style.css";
 
 const BACKEND_URL = import.meta.env.BACKEND_URL;
 
@@ -40,43 +34,6 @@ interface IPayload {
   };
 }
 
-function customToast(payload: IPayload) {
-  const startDateObject = payload?.date_range?.from;
-  const endDateObject = payload?.date_range?.to;
-
-  if (
-    startDateObject.date !== undefined &&
-    startDateObject.month !== undefined &&
-    startDateObject.year !== undefined &&
-    endDateObject.date !== undefined &&
-    endDateObject.month !== undefined &&
-    endDateObject.year !== undefined
-  ) {
-    const startDate = new Date(
-      startDateObject.year,
-      startDateObject.month,
-      startDateObject.date
-    );
-    const endDate = new Date(
-      endDateObject.year,
-      endDateObject.month,
-      endDateObject.date
-    );
-
-    toast("Your Tablet Course has been Registered", {
-      description: `Tablet Course from ${startDate.toLocaleString("default", {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-      })} to ${endDate.toLocaleString("default", {
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-      })}`,
-    });
-  }
-}
-
 function Form() {
   const [time, setTime] = useState<Date>();
   const [tablet, setTablet] = useState<string>();
@@ -86,78 +43,56 @@ function Form() {
     to: addDays(new Date(), 0),
   });
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const payload = {
-      tablet,
-      date_range: {
-        from: {
-          date: date?.from?.getDate(),
-          month: date?.from?.getMonth(),
-          year: date?.from?.getFullYear(),
-        },
-        to: {
-          date: date?.to?.getDate(),
-          month: date?.to?.getMonth(),
-          year: date?.to?.getFullYear(),
-        },
-      },
-      period,
-      time: {
-        hour: time?.getHours(),
-        min: time?.getMinutes(),
-      },
-    };
-
-    await axios
-      .post(`${BACKEND_URL}/api/v1/randomShit`, payload)
-      .then(() => customToast(payload));
+  const tabletFormProps = {
+    time,
+    setTime,
+    tablet,
+    setTablet,
+    period,
+    setPeriod,
+    date,
+    setDate,
   };
+
+  useEffect(() => {
+    async () => {
+      await axios
+        .get<IPayload[]>(`${BACKEND_URL}/api/v1/randomShit2`)
+        .then((res) => setPayload(res.data));
+    };
+  }, []);
+
+  const [payload, setPayload] = useState<IPayload[]>([]);
 
   return (
     <>
-      <div className="flex h-[85vh] flex-col justify-center items-center px-6 lg:px-8">
-        <div className="shadow shadow-[#00000017] bg-[#00000005] w-[50%] p-7">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img className="mx-auto h-10 w-auto" src={logo} alt="Logo" />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Enter the Tablet Details
-            </h2>
-          </div>
-
-          <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form onSubmit={handleSubmit} className="space-y-6">
-              <div className="grid max-w-sm items-center gap-1.5">
-                <Label htmlFor="tablet">Tablet Name</Label>
-                <Input
-                  defaultValue={tablet}
-                  onChange={(e) => setTablet(e.target.value)}
-                  type="tablet"
-                  id="email"
-                  placeholder="Tablet Name"
-                />
-              </div>
-              <div className="grid max-w-sm items-center gap-1.5">
-                <Label htmlFor="email">Course</Label>
-                <FormDateRange className="" date={date} setDate={setDate} />
-              </div>
-              <div className="grid max-w-sm items-center gap-1.5">
-                <Label htmlFor="email">Timings</Label>
-                <FormTimePicker setDate={setTime} date={time} />
-              </div>
-              <div className="grid max-w-sm items-center gap-1.5">
-                <Label htmlFor="email">Period</Label>
-                <FormSelect period={period} setPeriod={setPeriod} />
-              </div>
-              <div className="grid max-w-sm items-center gap-1.5">
-                <Button className="bg-[#212121] hover:bg-[#000] hover:text-[#44D9E6] hover:text-[0.95rem] transition-[font-size]">
-                  Create Register
-                </Button>
-              </div>
-            </form>
-          </div>
-        </div>
+      <div
+        data-place="form"
+        className="flex justify-center items-center gap-[5rem] py-[1.5rem] px-6 lg:px-8"
+      >
+        <TabletForm tabletFormProps={tabletFormProps} />
+        <section data-place="form" className="w-[30%] self-start">
+          <ScrollArea className="h-full w-full rounded-md border shadow shadow-[#00000017] bg-[#00000005]">
+            <div className="p-4">
+              <h4 className="mb-4 text-sm font-medium leading-none text-center">
+                Registered Tablets
+              </h4>
+              {payload.map((elem) => (
+                <>
+                  <div
+                    key={elem.tablet}
+                    className="text-sm flex justify-between items-center"
+                  >
+                    <h5>{elem.tablet}</h5>
+                    <h5>{`${elem.date_range.from.date}/${elem.date_range.from.month}/${elem.date_range.from.year}`}</h5>
+                    <h5>{`${elem.date_range.to.date}/${elem.date_range.to.month}/${elem.date_range.to.year}`}</h5>
+                  </div>
+                  <Separator className="my-2" />
+                </>
+              ))}
+            </div>
+          </ScrollArea>
+        </section>
       </div>
     </>
   );
