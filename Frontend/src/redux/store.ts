0cb7d85b46a4875250c8ddc2a646844a5e06bcc6
Soorya U <sooryau7@gmail.com>
2024-01-12 20:50:19 +0530

    fix #3: Removed Roference Slices
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sampleSlice from "./slices/sample";
 import formPayloadSlice from "./slices/formPayload";
 export const store = configureStore({
   reducer: {
-    sample: sampleSlice,
     formPayload: formPayloadSlice,
   },
 });
