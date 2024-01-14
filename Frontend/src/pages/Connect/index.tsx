040d07fac31944c35b94ab738aa00620fd4c2ba5
Soorya U <sooryau7@gmail.com>
2024-01-14 12:16:00 +0530

    fix #3: Fixed Connect Page Styles and Logic
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- Frontend/src/pages/Connect/index.tsx
+++ Frontend/src/pages/Connect/index.tsx
@@ -21,35 +21,42 @@ import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { IConnect } from "@/interface";
 
 import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
+
+import logo from "@/assets/icon.png";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 function Connect() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [messages, setMessages] = useState("");
+  const [messages, setMessages] = useState(
+    "Username has been Taken. Try to set another Username."
+  );
 
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    console.log(`${BACKEND_URL}/v1/signin`);
     const res = await axios.get<IConnect>(`${BACKEND_URL}/v1/signin`, {
       params: { username, password },
     });
 
-    console.log(res.data);
     if (res.data && "error" in res.data) {
       setMessages("Invalid Username or Password");
       setError(true);
-      const setOut = setTimeout(() => setError(false), 5000);
-      clearTimeout(setOut);
+      setTimeout(() => {
+        setError(false);
+      }, 5000);
     } else {
       setSuccess(true);
-      const setOut = setTimeout(() => setError(false), 5000);
-      clearTimeout(setOut);
+      setMessages("Login Successfull!");
+      setTimeout(() => {
+        setSuccess(false);
+      }, 5000);
       navigate("/");
     }
   };
@@ -63,44 +70,55 @@ function Connect() {
     if ("error" in res.data) {
       setError(true);
       setMessages("Username has been Taken. Try to set another Username.");
-      const setOut = setTimeout(() => setError(false), 5000);
-      clearTimeout(setOut);
+      setTimeout(() => {
+        setError(false);
+      }, 5000);
     } else {
       setSuccess(true);
-      const setOut = setTimeout(() => setError(false), 5000);
-      clearTimeout(setOut);
+      setMessages("Sign Up Successfull!");
+      setTimeout(() => {
+        setSuccess(false);
+      }, 5000);
       navigate("/");
     }
   };
 
   return (
     <>
-      {error ? (
-        <>
-          <Alert variant="destructive" className="w-[80%] absolute left-[10%]">
-            <ExclamationTriangleIcon className="h-4 w-4" />
-            <AlertTitle>Error</AlertTitle>
-            <AlertDescription>{messages}</AlertDescription>
-          </Alert>
-        </>
-      ) : (
-        <></>
-      )}
-      {success ? (
-        <>
-          <Alert
-            variant="default"
-            className="w-[80%] absolute left-[10%] bg-transparent"
-          >
-            <FontAwesomeIcon className="h-4 w-4" icon={faThumbsUp} />
-            <AlertTitle>Success</AlertTitle>
-            <AlertDescription>{messages}</AlertDescription>
-          </Alert>
-        </>
-      ) : (
-        <></>
-      )}
-      <main className="flex flex-col justify-center items-center h-[80vh]  ">
+      <main
+        data-place="connect"
+        className="flex flex-col justify-center items-center h-[85vh] gap-[2rem]"
+      >
+        {error && (
+          <>
+            <Alert
+              variant="destructive"
+              className="w-[80%] shadow shadow-[#00000017] bg-[#00000005]"
+            >
+              <ExclamationTriangleIcon className="h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>{messages}</AlertDescription>
+            </Alert>
+          </>
+        )}
+        {success && (
+          <>
+            <Alert
+              variant="default"
+              className="w-[80%] shadow shadow-[#00000017] bg-[#00000005]"
+            >
+              <FontAwesomeIcon className="h-4 w-4" icon={faThumbsUp} />
+              <AlertTitle>Success</AlertTitle>
+              <AlertDescription>{messages}</AlertDescription>
+            </Alert>
+          </>
+        )}
+        <div className="flex justify-center items-center sm:mx-auto sm:w-full sm:max-w-sm gap-[2rem]">
+          <img className="h-10 w-auto" src={logo} alt="Logo" />
+          <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Connect with Us
+          </h2>
+        </div>
         <Tabs defaultValue="login" className="w-inherit px-6">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="login">Login</TabsTrigger>
@@ -154,6 +172,15 @@ function Connect() {
                 </CardDescription>
               </CardHeader>
               <CardContent className="space-y-2">
+                <div className="space-y-1">
+                  <Label htmlFor="first-name">First Name</Label>
+                  <Input
+                    id="first-name"
+                    placeholder="Enter your First Name..."
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
                 <div className="space-y-1">
                   <Label htmlFor="username">Username</Label>
                   <Input
