ed01cf864009f214c4e2bddf3f44e66ecd7fe08c
Puneeth <pyd773@gmail.com>
2024-01-14 18:51:20 +0530

    feat #2 : CORS added

--- Backend/src/vitista/main.go
+++ Backend/src/vitista/main.go
@@ -5,12 +5,18 @@ import (
 	login "vitista/personal_healthcare_app/login"
 	model "vitista/personal_healthcare_app/runners"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	routeGroup := r.Group("/v1") 
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
+
+	routeGroup := r.Group("/v1")
 	{
 		routeGroup.GET("/home",home.GetHome)
 		routeGroup.GET("/modelProcessing",model.RunModel)
@@ -19,6 +25,8 @@ func main() {
 		routeGroup.GET("/signin",login.SignIn)
 		routeGroup.GET("/readcookie",login.ReadCookie)
 		routeGroup.GET("/getall",home.GetAll)
+		// routeGroup.GET("/fit",fit.GetFit)
+		// routeGroup.GET("/fit/callback",fit.GooglefitCallback)
 	}
 	r.Run("localhost:8080")
 }
