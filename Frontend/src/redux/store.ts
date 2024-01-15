3d3b6da6b1981d7f95f83058ad23e0a374d458bb
Soorya U <sooryau7@gmail.com>
2024-01-15 21:08:17 +0530

    feat #3: Added Spinner Slice
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import formPayloadSlice from "./slices/formPayload";
 import userStorageSlice from "./slices/userStorage";
+import loadingSpinnerSlice from "./slices/loadingSpinner";
 
 export const store = configureStore({
   reducer: {
     formPayload: formPayloadSlice,
     userStorage: userStorageSlice,
+    loadingSpinner: loadingSpinnerSlice,
   },
 });
 
