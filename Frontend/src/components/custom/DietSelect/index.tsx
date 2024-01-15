a3a1a556505865b8cf5b744d47380bfefdf1d1e4
Soorya U <sooryau7@gmail.com>
2024-01-15 08:31:35 +0530

    feat #3: Added Diet Form
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ Frontend/src/components/custom/DietSelect/index.tsx
@@ -0,0 +1,36 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function DietSelect({
+  activityLevel,
+  setActivityLevel,
+}: {
+  activityLevel: string;
+  setActivityLevel: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  return (
+    <Select
+      onValueChange={(val) => setActivityLevel(val)}
+      defaultValue={activityLevel}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select the Activity Level" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Activity Level</SelectLabel>
+          <SelectItem value="low">Low</SelectItem>
+          <SelectItem value="medium">Medium</SelectItem>
+          <SelectItem value="high">High</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
